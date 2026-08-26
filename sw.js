3bcdd5b3a642af76a5abf44e0ac4e933591edc2e
backtree-create <backtree121029@gmail.com>
2026-08-26 17:02:42 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * 注意：VERSION は meta.json の "version" と揃えること。
  *       内容を更新したらここも必ず書き換える。
  */
-const VERSION = "2.8.1";
+const VERSION = "2.8.3";
 const CACHE_NAME = `tokushi-guidebook-${VERSION}`;
 
 const PRECACHE = [
